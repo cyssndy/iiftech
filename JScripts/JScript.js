0d0788c5a71273dfc9280916202d305842958ebc
cyssndy <cyssndy@hotmail.com>
2015-04-21 14:14:22 -0400

    Update JScript.js

--- JScripts/JScript.js
+++ JScripts/JScript.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
 	var titleImgH = $("img.title-img").height();
 	var titleImgW = $("img.title-img").width();
 	var pageimageH = $("div.imageFrame").height();
+	var pageheaderH = $("div.pageHeader").height();
+	if (pageimageH < 300){
+		pageimageH = 300;
+	}
+	if (pageheaderH < 300){
+		pageheaderH = 300;
+	}
 	
 	$(".navBtn").hover(
 		function(){
@@ -27,12 +34,15 @@ $(document).ready(function(){
 	
 	$(window).resize(function(){
 		if ($(window).width() > 700){
+			$("div.imageFrame").height(pageimageH);
+			$("div.pageHeader").height(pageheaderH);
 			$(".headerNavBlock").show();
 			$("img.title-img").height(titleImgH);
 			$("img.title-img").width(titleImgW);
 		}
 		else{
 			$("div.imageFrame").height($(window).width()/960 * pageimageH );
+			$("div.pageHeader").height($(window).width()/960 * pageheaderH);
 			$(".headerNavBlock").hide();
 			$("img.title-img").height(titleImgH * 0.8);
 			$("img.title-img").width(titleImgW * 0.8);
